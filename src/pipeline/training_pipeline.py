e71c844ad85ca486ffa4cdea34e163fc01df0e79
Swaraj-6 <swarajkkd@gmail.com>
2023-05-31 12:09:06 +0530

    Modified training pipeline

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -7,14 +7,40 @@ from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_training import ModelTrainer
 
-if __name__ == '__main__':
-    obj = DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
+class TrainingPipeline:
+    def __init__(self):
+        pass
 
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    def model_train(self):
+        try:
+            obj = DataIngestion()
+            train_data_path, test_data_path = obj.initiate_data_ingestion()
 
-    model_trainer = ModelTrainer()
-    best_model, best_model_score = model_trainer.initiate_model_training(train_arr, test_arr)
+            data_transformation = DataTransformation()
+            train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+            model_trainer = ModelTrainer()
+            best_model, best_model_score = model_trainer.initiate_model_training(train_arr, test_arr)
+
+            return best_model, best_model_score
+
+        except Exception as e:
+            logging.info("Error occurred in model_training method")
+            CustomException(e, sys)
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     obj = DataIngestion()
+#     train_data_path, test_data_path = obj.initiate_data_ingestion()
+#
+#     data_transformation = DataTransformation()
+#     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+#
+#     model_trainer = ModelTrainer()
+#     best_model, best_model_score = model_trainer.initiate_model_training(train_arr, test_arr)
 
 
